c7939d7f09d97d19316bd38aa472ee6cfd8d152c
Johanes Zenn <johannes.zenn@icloud.com>
2020-03-21 14:35:57 +0100

    configurations

---
+++ configurations/grid_search/start_script.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+import time
+configurations = [c for c in os.listdir("./configurations/grid_search") if c.endswith(".yaml") and c.startswith("configuration")]
+for config in configurations:
+	command = ["sbatch", "./jobs/train.sbatch", os.path.join("./configurations/grid_search", config)]
+	print(command)
+	time.sleep(1)
