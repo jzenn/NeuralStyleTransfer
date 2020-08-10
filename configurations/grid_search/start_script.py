1bb10042a1a5752ad5280e9dd6f1b736b237c210
Johanes Zenn <johannes.zenn@icloud.com>
2020-08-10 18:53:36 +0200

    cleanup

--- configurations/grid_search/start_script.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import time
-configurations = [c for c in os.listdir("./configurations/grid_search") if c.endswith(".yaml") and c.startswith("configuration")]
-for config in configurations:
-	command = ["sbatch", "./jobs/train.sbatch", os.path.join("./configurations/grid_search", config)]
-	print(command)
-	subprocess.call(command)
-	time.sleep(1)
